fe1a9f2d55fc73758c7524e78ed4e52f13c0b301
Catalin Balan <catalin.balan@gmail.com>
2013-07-16 11:53:20 +0300

    Updated examples comments

--- README.md
+++ README.md
@@ -1,4 +1,78 @@
-aws-swf-fluent-php
-==================
+# aws-swf-fluent-php
 
 Glue code around aws-sdk-php to allow fluent workflows definition
+
+
+## Quick Example
+
+### Three steps workflow with decision task
+
+```php
+class QuickSimpleDomain extends Aws_Swf_Domain {
+
+    /**
+     * Simple workflow domain configuration.
+     *
+     * Mandatory swf objects to be defined here:
+     *  - swf domain name using setDomainName method
+     *  - swf workflows using addWorkflow method
+     *  - actions using workflow's 'to'/registerTask method
+     */
+    protected function configure() {
+        $this->setDomainName('threeStepsZenDomain');
+
+        /**
+         * threeStepsZen workflow :
+         *  - on start, execute stepOne
+         *  - if stepOne failed, execute stepFour. See evaluateStepOneResult method
+         *  - if stepOne succeeded, execute stepTwo
+         *  - if stepTwo succeeded, execute stepThree
+         *
+         * On any unhandled exception, workflow execution will terminate with FAIL_WORKFLOW_EXECUTION decision.
+         * Decision tasks can catch/handle previous activity fail/success.
+         */
+        $this->addWorkflow('threeStepsZen')
+            ->to('activity://stepOne')
+            ->to('decision://evaluateStepOneResult')
+            ->to('activity://stepTwo')
+            ->to('activity://stepThree')
+            ->registerTask('activity://stepFour', array('comment' => 'Optional step 4'));
+    }
+
+    public function stepOne($context)   { /* do something on activity workers.*/ }
+
+    public function evaluateStepOneResult($context) {
+        $lastEvent = $decisionHint->getLastEvent();
+        if ($lastEvent['eventType'] == Enum\EventType::ACTIVITY_TASK_FAILED) {
+            $decisionHint->setItem($this->getActivity('stepFour'));
+            $decisionHint->setDecisionType(Enum\DecisionType::SCHEDULE_ACTIVITY_TASK);
+        }
+    }
+
+    public function stepTwo($context)   { /* do something on activity workers.*/ }
+
+    public function stepThree($context) { /* do something on activity workers.*/ }
+
+    public function stepFour($context)  { /* do something on activity workers.*/ }
+}
+
+// $domain = new QuickSimpleDomain();
+// set aws swf client
+// $domain->setSwfClient(Aws\Swf\SwfClient::factory(array(
+//    'key' => 'AWS key',
+//    'secret' => 'AWS secret key',
+//    'region' => 'us-east-1'
+//)));
+
+// start decision worker
+// $domain->pollForDecisionTask();
+
+// start activity worker
+// $domain->pollForActivityTask();
+
+// start a workflow execution
+// $domain->startWorkflowExecution('threeStepsZen', 5, true);
+```
+
+### More examples
+* See examples folder for more details
--- examples/SimpleDomain.php
+++ examples/SimpleDomain.php
@@ -7,14 +7,37 @@ use Aws\Swf\Enum;
 
 class SimpleDomain extends Aws_Swf_Domain {
 
+    /**
+     * Simple workflow domain configuration.
+     *
+     * Mandatory swf objects to be defined here:
+     *  - swf domain name using setDomainName method
+     *  - swf workflows using addWorkflow method
+     *  - actions using workflow's 'to'/registerTask method
+     */
     protected function configure() {
         $this->setDomainName('threeStepsZenDomain');
+
+        /**
+         * SWF client configuration can be done also outside of this method, prior to
+         * startWorkflowExecution, pollForDecisionTask or pollForActivityTask calls.
+         */
         $this->setSwfClient(Aws\Swf\SwfClient::factory(array(
             'key' => 'AWS key',
             'secret' => 'AWS secret key',
             'region' => 'us-east-1'
         )));
 
+        /**
+         * threeStepsZen workflow :
+         *  - on start, execute stepOne
+         *  - if stepOne failed, execute stepFour. See evaluateStepOneResult method
+         *  - if stepOne succeeded, execute stepTwo
+         *  - if stepTwo succeeded, execute stepThree
+         *
+         * On any unhandled exception, workflow execution will terminate with FAIL_WORKFLOW_EXECUTION decision.
+         * Decision tasks can catch/handle previous activity fail/success.
+         */
         $this->addWorkflow('threeStepsZen')
             ->to('activity://stepOne')
             ->to('decision://evaluateStepOneResult')
@@ -23,14 +46,12 @@ class SimpleDomain extends Aws_Swf_Domain {
             ->registerTask('activity://stepFour', array('comment' => 'Optional step 4'));
     }
 
-    public function evaluateStepOneResult($context, $decisionHint) {
-        $lastEvent = $decisionHint->getLastEvent();
-        if ($lastEvent['eventType'] == Enum\EventType::ACTIVITY_TASK_FAILED) {
-            $decisionHint->setItem($this->getActivity('stepFour'));
-            $decisionHint->setDecisionType(Enum\DecisionType::SCHEDULE_ACTIVITY_TASK);
-        }
-    }
-
+    /**
+     * activity://stepOne executed by the activity workers
+     *
+     * @param $context
+     * @return mixed
+     */
     public function stepOne($context) {
         $input = $context->getInput();
 
@@ -41,6 +62,24 @@ class SimpleDomain extends Aws_Swf_Domain {
         return $input * 10;
     }
 
+    /**
+     * decision://evaluateStepOneResult executed by the decision workers
+     * @param $context
+     * @param $decisionHint
+     */
+    public function evaluateStepOneResult($context, $decisionHint) {
+        $lastEvent = $decisionHint->getLastEvent();
+        if ($lastEvent['eventType'] == Enum\EventType::ACTIVITY_TASK_FAILED) {
+            $decisionHint->setItem($this->getActivity('stepFour'));
+            $decisionHint->setDecisionType(Enum\DecisionType::SCHEDULE_ACTIVITY_TASK);
+        }
+    }
+
+    /**
+     * activity://stepTwo executed by the activity workers
+     * @param $context
+     * @return mixed
+     */
     public function stepTwo($context) {
         $input = $context->getInput();
 
@@ -50,6 +89,11 @@ class SimpleDomain extends Aws_Swf_Domain {
         return $input * 20;
     }
 
+    /**
+     * activity://stepThree executed by the activity workers
+     * @param $context
+     * @return mixed
+     */
     public function stepThree($context) {
         $input = $context->getInput();
 
@@ -59,6 +103,11 @@ class SimpleDomain extends Aws_Swf_Domain {
         return $input * 30;
     }
 
+    /**
+     * activity://stepFour executed by the activity workers
+     * @param $context
+     * @return mixed
+     */
     public function stepFour($context) {
         $input = $context->getInput();
 
